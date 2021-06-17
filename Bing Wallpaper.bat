d06c2784af6bd5a18c9f30d458bccf003d297964
Hyukho Song <hyukho83@gmail.com>
2021-06-17 13:42:56 +0900

    updated

--- Bing Wallpaper.bat
+++ Bing Wallpaper.bat
@@ -1,6 +1,2 @@
-# Windows10 Login Background Downloader
-
-```
 robocopy "%UserProfile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "%UserProfile%\Desktop\Bing Wallpaper" /MIN:102400 /M
 rename "%UserProfile%\Desktop\Bing Wallpaper\*" "*.jpg"
-```
